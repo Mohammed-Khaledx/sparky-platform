1e365e9b6a9461285ea4574380c9aa197bf8307a
Mohammed-Khaledx <mohammedkhaledx1@gmail.com>
2025-04-30 10:49:06 +0300

    deploy

---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+// src/environments/environment.prod.ts
+export const environment = {
+    production: true,
+    apiUrl: 'https://your-backend-domain.vercel.app',
+    socketUrl: 'https://your-backend-domain.vercel.app'
+  };
