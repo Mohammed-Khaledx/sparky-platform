d170b3e97d357b59e78abc6956f3e588b3a149ef
Mohammed-Khaledx <mohammedkhaledx1@gmail.com>
2025-04-30 22:01:23 +0300

    vercel-front-deploy

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 // src/environments/environment.prod.ts
 export const environment = {
     production: true,
-    apiUrl: 'sparky-platform-production.up.railway.app',
-    socketUrl: 'sparky-platform-production.up.railway.app'
+    apiUrl: 'https://sparky-platform-production.up.railway.app',
+    socketUrl: 'https://sparky-platform-production.up.railway.app'
   };
